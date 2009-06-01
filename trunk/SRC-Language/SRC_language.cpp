a6c81e0c45152bb0702450d204004e9a66281ee9
daidodo <daidodo@2848b288-aaed-11dd-a772-7fcde58d38e6>
2009-06-01 10:17:27 +0000

    1. 增加dll宏 2. 修正ErrorMsg函数

--- trunk/SRC-Language/SRC_language.cpp
+++ trunk/SRC-Language/SRC_language.cpp
@@ -1,22 +1,22 @@
 #include "SRC_language.h"
 #include "global.h"
 
-void SRC_init()
+SRCDLL_API void SRC_init()
 {
     global().Init();
 }
 
-bool SRC_compile(const char * filename)
+SRCDLL_API bool SRC_compile(const char * filename)
 {
     return global().Compile(filename);
 }
 
-bool SRC_run(int argc,const char * const * argv)
+SRCDLL_API bool SRC_run(int argc,const char * const * argv)
 {
     return global().Run(argc,argv);
 }
 
-bool SRC_register_function(const char * func_name,__SRC_UserFunc func_ptr,unsigned int dst_len_max)
+SRCDLL_API bool SRC_register_function(const char * func_name,__SRC_UserFunc func_ptr,unsigned int dst_len_max)
 {
     return global().AddFunc(func_name,func_ptr,dst_len_max);
 }
